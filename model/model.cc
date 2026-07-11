9ac3d0b8261adbfe5253a0dda93fd1c51955f3e7
elprofesoriqo <igorjankowwski@gmail.com>
2026-07-11 15:09:47 +0200

    refactor(model): drop cross-attention stub and dead Model members

--- model/model.cc
+++ model/model.cc
@@ -1,11 +1,9 @@
 #include "model.h"
-#include "cpu/attention.h"
+
 #include "cpu/linear.h"
 
-void Model::execute() {
-    std::shared_ptr<Attention> attention = std::make_shared<CPUAttention>();
-    std::shared_ptr<Linear> linear = std::make_shared<CPULinear>();
-
-    attention->compute();
-    linear->compute();
+void Model::execute()
+{
+  std::shared_ptr<Linear> linear = std::make_shared<CPULinear>();
+  linear->compute();
 }
