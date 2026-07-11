b7030610a6fd2879b136168dd8a2e2f88b43a0a8
elprofesoriqo <igorjankowwski@gmail.com>
2026-07-11 15:09:18 +0200

    style: apply clang-format and clang-tidy to existing sources

--- api/engine.h
+++ api/engine.h
@@ -1,17 +1,19 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include <memory>
 #include "../model/model.h"
 
-class Engine {
-public:
-    Engine();
+#include <memory>
 
-    void run();
+class Engine
+{
+public:
+  Engine();
+
+  void run();
 
 private:
-    std::shared_ptr<Model> model;
+  std::shared_ptr<Model> model{std::make_shared<Model>()};
 };
 
 #endif // ENGINE_H
